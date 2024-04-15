9a38004513005257c654816a92fc17f5cdeac702
Ivan Germanov <ivan@post.news>
2024-04-15 19:19:25 +0200

    Simplify imports

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .dataset_augmenter import DatasetAugmenter
+from .feature_augmenter import FeatureAugmenter
+from .name_augmenter import NameAugmenter
+from .scrapy_augmenter import ScrapyAugmenter
+from .similarity_augmenter import SimilarityAugmenter
+from .tag_augmenter import TagAugmenter
